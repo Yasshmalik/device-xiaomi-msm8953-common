8ced3516e37c64703eb447f96b9395e55d917b1a
Jay-b <34432542+jayant-b@users.noreply.github.com>
2018-05-08 13:08:41 +0530

    Updated for oxygen

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -43,7 +43,9 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
+TARGET_KERNEL_SOURCE := kernel/xiaomi/oxygen
+TARGET_KERNEL_CONFIG := lineageos_oxygen_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
